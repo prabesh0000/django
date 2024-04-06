e2e76684a78058fae2cef8f41c347ae4c0045f7c
prabesh0000 <123892028+prabesh0000@users.noreply.github.com>
2024-04-06 23:49:26 +0545

    Update views.py

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render , HttpResponse
 
 def index(request):
     return HttpResponse ("this is home page")
+def about (request):
+    return HttpResponse ("this is about  page")
+def services (request):
+    return HttpResponse ("this is serivices page")
+def contact (request):
+    return HttpResponse ("this is contact  page")
