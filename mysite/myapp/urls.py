1ff1b78c936cfcc8257f01ab65d5a4a38d8e9d5d
prabesh0000 <123892028+prabesh0000@users.noreply.github.com>
2024-04-06 23:49:17 +0545

    Update urls.py

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -3,7 +3,8 @@ from django.urls import path , include
 from myapp import views
 
 urlpatterns = [
- 
- path ('', views.index ,  name = 'myapp')
-
+ path ('', views.index ,  name = 'myapp'),
+ path ('services', views.services ,  name = 'myapp'),
+ path ('about', views.about ,  name = 'about'),
+ path ('contact', views.about ,  name = 'about')
 ]
