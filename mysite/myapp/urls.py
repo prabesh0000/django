151f614c05b5e045eba8814ea48b00e4580039fb
prabesh0000 <123892028+prabesh0000@users.noreply.github.com>
2024-04-08 17:38:14 +0545

    learned to add static files in website

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path , include
 from myapp import views
-
+# urls dispatcher process
 urlpatterns = [
  path ('', views.index ,  name = 'myapp'),
  path ('services', views.services ,  name = 'myapp'),
