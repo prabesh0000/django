53d0eeaf33ac08513db2564895abb9de8a57f705
prabesh0000 <123892028+prabesh0000@users.noreply.github.com>
2024-04-12 14:24:10 +0545

    CHANGED ADMIN PANEL NAMES AND PORTAL NAMES

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path , include
 
+admin.site.site_header = "PRABESH Admin"
+admin.site.site_title = "PRABESH PORTAL"
+admin.site.index_title = "Welcome to PRABESH Portal"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path ('', include('myapp.urls'))
