0a9cadafcacf05c651583e8141015d206313d872
prabesh0000 <123892028+prabesh0000@users.noreply.github.com>
2024-04-14 20:12:21 +0545

    added nav to all the pages using base.html

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -12,8 +12,8 @@ def index(request):
     }
     return render (request, 'index.html' , context)
 def about (request):
-    return HttpResponse ("this is about  page")
+    return render  (request, 'about.html')
 def services (request):
-    return HttpResponse ("this is serivices page")
+    return render ( request, 'services.html')
 def contact (request):
-    return HttpResponse ("this is contact  page")
+    return render( request , 'contact')
